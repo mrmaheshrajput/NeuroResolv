107408c6b0df612c9f3c3504a9e8f414fb5bf5c8
Mahesh Rajput <maheshrajput22@gmail.com>
2026-01-20 21:34:35 +0530

    [11] Stable - Fix alembic env file, remove chromadb and related code

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -14,7 +14,7 @@ from app.config import get_settings
 config = context.config
 
 settings = get_settings()
-config.set_main_option("sqlalchemy.url", settings.database_url.replace("sqlite+aiosqlite", "sqlite"))
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
